5cd5846943bdba7f8bf1dec82c2e0e61807e09db
Shashwat Shekhar <svs6603@psu.edu>
2021-01-11 16:41:00 +0530

    Use Scaffold()

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello!"),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("This is my app bar"),
+      ),
+      body: Text("This is my main body"),
+    ));
   }
 }
